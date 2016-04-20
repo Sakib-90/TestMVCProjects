06b831ae193c8ac922f41edaced5542199780b4d
Tareq <tareq021@gmail.com>
2016-04-21 04:10:22 +0600

    Teacher Restructured

--- SaveEmployee/SaveEmployee/Models/Person.cs
+++ SaveEmployee/SaveEmployee/Models/Person.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace UniversityApplication.Models
 {
@@ -8,6 +9,7 @@ namespace UniversityApplication.Models
         [Key]
         [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
+        [DisplayName("Contact No.")]
         public string Contact { get; set; }
         public string Address { get; set; }
         public string Department { get; set; }
--- SaveEmployee/SaveEmployee/Models/Teacher.cs
+++ SaveEmployee/SaveEmployee/Models/Teacher.cs
@@ -1,10 +1,12 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace UniversityApplication.Models
 {
     public class Teacher : Person
     {
         public string Designation { get; set; }
+        [DisplayName("Credit to be taken")]
         public int Credit { get; set; }
     }
 }
