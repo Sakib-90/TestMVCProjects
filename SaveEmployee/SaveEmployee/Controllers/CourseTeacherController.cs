82858cc54b424a759593834358a17a9fbe26fd7b
Tareq <tareq021@gmail.com>
2016-04-21 23:18:37 +0600

    Cascading DDL department teacher works fine

--- SaveEmployee/SaveEmployee/Controllers/CourseTeacherController.cs
+++ SaveEmployee/SaveEmployee/Controllers/CourseTeacherController.cs
@@ -4,133 +4,104 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using UniversityApplication.BLL;
+using UniversityApplication.Context;
+using UniversityApplication.Models;
 
 namespace UniversityApplication.Controllers
 {
     public class CourseTeacherController : Controller
     {
-        DepartmentManager departmentManager = new DepartmentManager();
-
-        TeacherManager teacherManager = new TeacherManager();
-
-        CourseManager courseManager = new CourseManager();
-
+        
         // GET: CourseTeacher
-        public ActionResult CourseTeacher()
+        public ActionResult AssignCourse()
         {
-            //GenerateDropDownValue();
-            var departments = departmentManager.GetDepartments();
-            
-            List<SelectListItem> departmentList = new List<SelectListItem>();
 
-            foreach (var department in departments)
+
+            List<Department> allDepartments = new List<Department>();
+            List<Teacher> allTeachers = new List<Teacher>();
+
+            using (ApplicationContext db = new ApplicationContext())
             {
-                departmentList.Add(
-
-                    new SelectListItem()
-                    {
-                        Value = department.Name,
-                        Text = department.Name
-                    }
-                    );
+                allDepartments = db.Departments.OrderBy(a => a.Name).ToList();
             }
 
-            ViewBag.Departments = departmentList;
+            ViewBag.Departments = new SelectList(allDepartments, "Name", "Name");
+            ViewBag.TeachersName = new SelectList(allTeachers,"Name","Name");
 
             return View();
         }
 
         [HttpPost]
-        public ActionResult GetTeacher(string departmentName)
+        [ValidateAntiForgeryToken]
+        public ActionResult AssignCourse(CourseTeacher courseTeacher)
         {
-            var teachersList = teacherManager.GetTeachers(departmentName);
+            List<Department> allDepartments = new List<Department>();
+            List<Teacher> allTeachers = new List<Teacher>();
 
-            List<SelectListItem> teacherNameList = teachersList.Select(teacher => new SelectListItem()
+            using (ApplicationContext db = new ApplicationContext())
             {
-                Value = teacher.Name, Text = teacher.Name
-            }).ToList();
+                allDepartments = db.Departments.OrderBy(a => a.Name).ToList();
 
-            ViewBag.TeachersName = teacherNameList;
+                if (courseTeacher != null && courseTeacher.Teacher != null)
+                {
+                    allTeachers =
+                        db.Teachers.Where(a => a.Department.Equals(courseTeacher.Department))
+                            .OrderBy(a => a.Name)
+                            .ToList();
+                }
+            }
 
-            return Json(teacherNameList, JsonRequestBehavior.AllowGet);
+            ViewBag.Departments = new SelectList(allDepartments, "Name", "Name",courseTeacher.Department);
+            ViewBag.TeachersName = new SelectList(allTeachers, "Name", "Name",courseTeacher.Teacher);
+
+            if (ModelState.IsValid)
+            {
+                using (ApplicationContext db = new ApplicationContext())
+                {
+                    db.CoursesTeachers.Add(courseTeacher);
+                    db.SaveChanges();
+                    ModelState.Clear();
+                    courseTeacher = null;
+                    //ViewBag.Message = "Successfully submitted";
+                }
+            }
+            else
+            {
+                //ViewBag.Message = "Failed! Please try again";
+            }
+            return View(courseTeacher);
+
         }
 
-
-
-
-
-
-        //private void GenerateDropDownValue()
-        //{
-        //    var departments = departmentManager.GetDepartments();
-
-        //    List<SelectListItem> departmentList = new List<SelectListItem>();
-
-        //    foreach (var department in departments)
-        //    {
-        //        departmentList.Add(
-
-        //            new SelectListItem()
-        //            {
-        //                Value = department.Name,
-        //                Text = department.Name
-        //            }
-        //            );
-        //    }
-
-        //    ViewBag.Departments = departmentList;
-
-        //    var teachersList = teacherManager.GetTeachers();
-
-        //    List<SelectListItem> teacherNameList = new List<SelectListItem>();
-
-        //    foreach (var teacher in teachersList)
-        //    {
-        //        teacherNameList.Add(
-
-        //            new SelectListItem()
-        //            {
-        //                Value = teacher.Name,
-        //                Text = teacher.Name
-        //            }
-        //            );
-        //    }
-
-        //    ViewBag.TeachersName = teacherNameList;
-
-        //    //List<SelectListItem> teacherCreditList = new List<SelectListItem>();
-
-        //    //foreach (var teacher in teachersList)
-        //    //{
-        //    //    teacherCreditList.Add(
-
-        //    //        new SelectListItem()
-        //    //        {
-        //    //            Value = teacher.Credit.ToString(),
-        //    //            Text = teacher.Credit.ToString()
-        //    //        }
-        //    //        );
-        //    //}
-
-        //    //ViewBag.TeachersCredit = teacherCreditList; 
-
-        //    var courseList = courseManager.GetCourses();
-
-        //    List<SelectListItem> courseCodeList = new List<SelectListItem>();
-
-        //    foreach (var course in courseList)
-        //    {
-        //        courseCodeList.Add(
-
-        //            new SelectListItem()
-        //            {
-        //                Value = course.Code,
-        //                Text = course.Code
-        //            }
-        //            );
-        //    }
-
-        //    ViewBag.CourseCode = courseCodeList;
-        //}
+        [HttpGet]
+        public JsonResult GetTeachers(string departmentName)
+        {
+            List<Teacher> allTeachers = new List<Teacher>();
+           
+            if (departmentName!=null)
+            {
+                using (ApplicationContext db = new ApplicationContext())
+                {
+                    allTeachers = db.Teachers.Where(a => a.Department.Equals(departmentName)).OrderBy(a => a.Name).ToList();
+                }
+            }
+            if (Request.IsAjaxRequest())
+            {
+                return new JsonResult
+                {
+                    Data = allTeachers,
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
+            else
+            {
+                return new JsonResult
+                {
+                    Data = "Not valid request",
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
+        }
+        
     }
 }
