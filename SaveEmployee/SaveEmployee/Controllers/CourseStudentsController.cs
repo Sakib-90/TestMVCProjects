40dc69091a8707349902859ffe07b207f7b6902f
Tareq <tareq021@gmail.com>
2016-04-26 23:25:47 +0600

    Enroll in a Course, Dropdown for Student Reg is working good

---
+++ SaveEmployee/SaveEmployee/Controllers/CourseStudentsController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UniversityApplication.Context;
+using UniversityApplication.Models;
+
+namespace SaveEmployee.Controllers
+{
+    public class CourseStudentsController : Controller
+    {
+        private ApplicationContext db = new ApplicationContext();
+        
+        // GET: CourseStudents/Create
+        public ActionResult StudentToCourse()
+        {
+            List<Student> allRegisteredStudents = new List<Student>();
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                allRegisteredStudents = db.Students.OrderBy(a=>a.StudentRegNo).ToList();
+            }
+            ViewBag.Students = new SelectList(allRegisteredStudents, "StudentRegNo", "StudentRegNo");
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult StudentToCourse([Bind(Include = "CourseStudentID,CourseStudentRegNo,CourseStudentName,CourseStudentEmail,CourseStudentDepartmentCode,CourseStudentCourse,CourseStudentRegDate")] CourseStudent courseStudent)
+        {
+            List<Student> allRegisteredStudents = new List<Student>();
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                allRegisteredStudents = db.Students.OrderBy(a => a.StudentRegNo).ToList();
+            }
+            ViewBag.Students = new SelectList(allRegisteredStudents, "StudentRegNo", "StudentRegNo", courseStudent.CourseStudentRegNo);
+
+            if (ModelState.IsValid)
+            {
+                db.CoursesStudents.Add(courseStudent);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(courseStudent);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- SaveEmployee/SaveEmployee/Models/CourseStudent.cs
+++ SaveEmployee/SaveEmployee/Models/CourseStudent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -8,11 +9,18 @@ namespace UniversityApplication.Models
 {
     public class CourseStudent
     {
+        [DisplayName("Student Reg. No.")]
         public string CourseStudentRegNo { get; set; }
+        [DisplayName("Name")]
         public string CourseStudentName { get; set; }
+        [DisplayName("Email")]
         public string CourseStudentEmail { get; set; }
+        [DisplayName("Department")]
         public string CourseStudentDepartmentCode { get; set; }
+        [DisplayName("Select Course")]
         public string CourseStudentCourse { get; set; }
+        [DisplayName("Date")]
+        [DataType(DataType.Date)]
         public DateTime CourseStudentRegDate { get; set; }
         [Key]
         public int CourseStudentID { get; set; }
