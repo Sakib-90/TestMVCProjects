24075316315131083d9da25d173acd7aa0895527
Tareq <tareq021@gmail.com>
2016-04-26 19:12:32 +0600

    Class Room in next branch

--- SaveEmployee/SaveEmployee/Models/Classroom.cs
+++ SaveEmployee/SaveEmployee/Models/Classroom.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -8,12 +9,20 @@ namespace UniversityApplication.Models
 {
     public class Classroom
     {
+        [DisplayName("Department")]
         public string ClassRoomDepartmentCode { get; set; }
+        [DisplayName("Course")]
         public string ClassRoomCourseID { get; set; }
         [Key]
+        [DisplayName("Room No")]
         public string ClassRoomRoomNo { get; set; }
+        [DisplayName("Day")]
         public string ClassRoomWeekDay { get; set; }
+        [DisplayName("From")]
+        [DataType(DataType.Time)]
         public TimeSpan ClassRoomStartsAt { get; set; }
+        [DisplayName("To")]
+        [DataType(DataType.Time)]
         public TimeSpan ClassRoomEndssAt { get; set; }
     }
 }
