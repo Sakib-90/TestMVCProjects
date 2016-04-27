668a49f91b703648b0d82aa196f92a76efeaa894
Tareq <tareq021@gmail.com>
2016-04-27 19:10:03 +0600

    Enroll Course to a student working fine

--- SaveEmployee/SaveEmployee/Controllers/CourseStudentsController.cs
+++ SaveEmployee/SaveEmployee/Controllers/CourseStudentsController.cs
@@ -19,11 +19,22 @@ namespace SaveEmployee.Controllers
         public ActionResult StudentToCourse()
         {
             List<Student> allRegisteredStudents = new List<Student>();
+            List<Course> allCourses = new List<Course>();
+
+            string studentName = "";
+            string studentEmail = "";
+            string studentDepartment = "";
+
             using (ApplicationContext db = new ApplicationContext())
             {
                 allRegisteredStudents = db.Students.OrderBy(a=>a.StudentRegNo).ToList();
             }
             ViewBag.Students = new SelectList(allRegisteredStudents, "StudentRegNo", "StudentRegNo");
+            ViewBag.CourseCode = new SelectList(allCourses, "CourseCode", "CourseName");
+            ViewBag.StudentName = studentName;
+            ViewBag.StudentEmail = studentEmail;
+            ViewBag.StudentDepartment = studentDepartment;
+
             return View();
         }
 
@@ -33,22 +44,154 @@ namespace SaveEmployee.Controllers
         public ActionResult StudentToCourse([Bind(Include = "CourseStudentID,CourseStudentRegNo,CourseStudentName,CourseStudentEmail,CourseStudentDepartmentCode,CourseStudentCourse,CourseStudentRegDate")] CourseStudent courseStudent)
         {
             List<Student> allRegisteredStudents = new List<Student>();
+            List<Course> allCourses = new List<Course>();
+            string studentName = "";
+            string studentEmail = "";
+            string studentDepartment = "";
+
             using (ApplicationContext db = new ApplicationContext())
             {
                 allRegisteredStudents = db.Students.OrderBy(a => a.StudentRegNo).ToList();
             }
             ViewBag.Students = new SelectList(allRegisteredStudents, "StudentRegNo", "StudentRegNo", courseStudent.CourseStudentRegNo);
+            ViewBag.CourseCode = new SelectList(allCourses, "CourseCode", "CourseName",courseStudent.CourseStudentCourse);
+            ViewBag.StudentName = studentName;
+            ViewBag.StudentEmail = studentEmail;
+            ViewBag.StudentDepartment = studentDepartment;
 
             if (ModelState.IsValid)
             {
                 db.CoursesStudents.Add(courseStudent);
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                //return RedirectToAction("Index");
+                return RedirectToAction("StudentToCourse");
             }
 
             return View(courseStudent);
         }
 
+
+        public JsonResult GetStudentName(string studentRegNo)
+        {
+            string name = "";
+
+            if (!string.IsNullOrEmpty(studentRegNo))
+            {
+                using (ApplicationContext db = new ApplicationContext())
+                {
+                    name = db.Students.Where(c => c.StudentRegNo == studentRegNo).Select(p => p.StudentName).Single();
+                }
+            }
+            if (Request.IsAjaxRequest())
+            {
+                return new JsonResult
+                {
+                    Data = name,
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
+            else
+            {
+                return new JsonResult
+                {
+                    Data = "Not valid request",
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
+        }
+
+        public JsonResult GetStudentEmail(string studentRegNo)
+        {
+            string email = "";
+
+            if (!string.IsNullOrEmpty(studentRegNo))
+            {
+                using (ApplicationContext db = new ApplicationContext())
+                {
+                    email = db.Students.Where(c => c.StudentRegNo == studentRegNo).Select(p => p.StudentEmail).Single();
+                }
+            }
+            if (Request.IsAjaxRequest())
+            {
+                return new JsonResult
+                {
+                    Data = email,
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
+            else
+            {
+                return new JsonResult
+                {
+                    Data = "Not valid request",
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
+        }
+
+        public JsonResult GetStudentDepartment(string studentRegNo)
+        {
+            string departmentCode = "";
+            string department = "";
+
+            if (!string.IsNullOrEmpty(studentRegNo))
+            {
+                using (ApplicationContext db = new ApplicationContext())
+                {
+                    departmentCode = db.Students.Where(c => c.StudentRegNo == studentRegNo).Select(p => p.StudentDepartmentCode).Single();
+                    department = db.Departments.Where(c => c.DepartmentCode == departmentCode).Select(p => p.DepartmentName).Single();
+                }
+            }
+            if (Request.IsAjaxRequest())
+            {
+                return new JsonResult
+                {
+                    Data = department,
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
+            else
+            {
+                return new JsonResult
+                {
+                    Data = "Not valid request",
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
+        }
+
+        public JsonResult GetCourseCode(string departmentName)
+        {
+            List<Course> allCourses = new List<Course>();
+
+            if (departmentName != null)
+            {
+                using (ApplicationContext db = new ApplicationContext())
+                {
+                    string departmentCode =
+                        db.Students.Where(p => p.StudentRegNo.Equals(departmentName))
+                            .Select(d => d.StudentDepartmentCode)
+                            .Single();
+                    allCourses = db.Courses.Where(a => a.CourseDepartmentCode.Equals(departmentCode)).OrderBy(a => a.CourseName).ToList();
+                }
+            }
+            if (Request.IsAjaxRequest())
+            {
+                return new JsonResult
+                {
+                    Data = allCourses,
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
+            else
+            {
+                return new JsonResult
+                {
+                    Data = "Not valid request",
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+            }
+        }
 
         protected override void Dispose(bool disposing)
         {
