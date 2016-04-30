9db708b4e03821522a951364a1aeee57d0f44942
Tareq <tareq021@gmail.com>
2016-04-30 14:48:23 +0600

    View Courses Stat initiated

---
+++ SaveEmployee/SaveEmployee/Controllers/CourseStaticssController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UniversityApplication.Context;
+using UniversityApplication.Models;
+
+namespace UniversityApplication.Controllers
+{
+    public class CourseStaticssController : Controller
+    {
+        // GET: CourseStaticss
+        public ActionResult CourseStatistics()
+        {
+            List<Department> allDepartments = new List<Department>();
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                allDepartments = db.Departments.OrderBy(a => a.DepartmentName).ToList();
+            }
+            ViewBag.Departments = new SelectList(allDepartments, "DepartmentCode", "DepartmentName");
+
+            return View();
+        }
+    }
+}
