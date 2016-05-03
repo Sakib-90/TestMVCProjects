30be615bce4787404da9e8b67e0d94ebb9617473
Tareq <tareq021@gmail.com>
2016-05-03 19:45:55 +0600

    Updated Database Backup on May 3, 2016

--- SaveEmployee/SaveEmployee/Controllers/StudentResultsController.cs
+++ SaveEmployee/SaveEmployee/Controllers/StudentResultsController.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Web;
 using System.Web.Mvc;
 using UniversityApplication.BLL;
 using UniversityApplication.Context;
@@ -17,26 +12,9 @@ namespace UniversityApplication.Controllers
         private ApplicationContext db = new ApplicationContext();
         ResultManager resultManager = new ResultManager();
 
-        // GET: StudentResults/Create
         public ActionResult SaveResult()
         {
-            //List<Student> allRegisteredStudents = new List<Student>();
-            //List<CourseStudent> allCourses = new List<CourseStudent>();
             GenerateDropDownValue();
-
-            //string studentName = "";
-            //string studentEmail = "";
-            //string studentDepartment = "";
-
-            //using (ApplicationContext db = new ApplicationContext())
-            //{
-            //    allRegisteredStudents = db.Students.OrderBy(a => a.StudentRegNo).ToList();
-            //}
-            //ViewBag.Students = new SelectList(allRegisteredStudents, "StudentRegNo", "StudentRegNo");
-            //ViewBag.CourseCode = new SelectList(allCourses, "CourseStudentCourse", "CourseStudentCourse");
-            //ViewBag.StudentName = studentName;
-            //ViewBag.StudentEmail = studentEmail;
-            //ViewBag.StudentDepartment = studentDepartment;
 
             return View();
         }
@@ -47,28 +25,10 @@ namespace UniversityApplication.Controllers
         {
             GenerateDropDownValue();
 
-            //List<Student> allRegisteredStudents = new List<Student>();
-            //List<Course> allCourses = new List<Course>();
-            //string studentName = "";
-
-            //string studentEmail = "";
-            //string studentDepartment = "";
-
-            //using (ApplicationContext db = new ApplicationContext())
-            //{
-            //    allRegisteredStudents = db.Students.OrderBy(a => a.StudentRegNo).ToList();
-            //}
-            //ViewBag.Students = new SelectList(allRegisteredStudents, "StudentRegNo", "StudentRegNo", studentResult.StudentResultRegNo);
-            //ViewBag.CourseCode = new SelectList(allCourses, "CourseStudentCourse", "CourseStudentCourse", studentResult.StudentResultCourse);
-            //ViewBag.StudentName = studentName;
-            //ViewBag.StudentEmail = studentEmail;
-            //ViewBag.StudentDepartment = studentDepartment;
-
-            if (ModelState.IsValid)
+           if (ModelState.IsValid)
             {
                 db.StudentResults.Add(studentResult);
                 db.SaveChanges();
-                //return RedirectToAction("Index");
                 return RedirectToAction("SaveResult");
             }
 
@@ -212,7 +172,7 @@ namespace UniversityApplication.Controllers
                 };
             }
         }
-        public JsonResult GetCourseCode(string departmentName)// REG NO IS COMING AS PARAMETER
+        public JsonResult GetCourseCode(string departmentName)
         {
             List<CourseStudent> allCourses = new List<CourseStudent>();
 
@@ -220,11 +180,6 @@ namespace UniversityApplication.Controllers
             {
                 using (ApplicationContext db = new ApplicationContext())
                 {
-                    //string departmentCode =
-                    //    db.CoursesStudents.Where(p => p.StudentRegNo.Equals(departmentName))
-                    //        .Select(d => d.StudentDepartmentCode)
-                    //        .Single();
-                    //allCourses = db.CoursesStudents.Where(a => a.CourseStudentRegNo.Equals(departmentName)).Select(s=>s.CourseStudentCourse).ToList();
                     allCourses = db.CoursesStudents.Where(a => a.CourseStudentRegNo.Equals(departmentName)).OrderBy(p=>p.CourseStudentCourse).ToList();
                 }
             }
